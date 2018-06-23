1b6a7f05a7c44178cd258ffec95fa7836e2321b5
Tongjian Cui <67202237+elseifn@users.noreply.github.com>
2018-06-23 13:27:28 +0800

    Formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,9 @@ import mind.Santini;
 import model.MindData;
 
 public class Main {
-  public static void main(String[] args) {
-    Santini dolores = new Santini();
-    Santini.playSweetWater();
-    MindData theCradle = dolores.gatherData();
-  }
+	public static void main(String[] args) {
+		Santini dolores = new Santini();
+		Santini.playSweetWater();
+		MindData theCradle = dolores.gatherData();
+	}
 }
