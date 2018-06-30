76bf2509343c8c7adc21d830f0382c479668c245
Tongjian Cui <67202237+elseifn@users.noreply.github.com>
2018-06-30 12:32:17 +0800

    Added class to help with data prediction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
 import mind.Santini;
-import model.MindData;
+import model.data.MindData;
+import model.data.PredictionData;
 
 public class Main {
 	public static void main(String[] args) {
 		Santini dolores = new Santini();
-		Santini.playSweetWater();
+		//Santini.playSweetWater();
 		MindData theCradle = dolores.gatherData();
+		PredictionData predictionData = new PredictionData(theCradle);
+		predictionData.calculatePredictionData();
 	}
 }
---
+++ src/main/java/model/data/MindData.java
@@ -0,0 +1,47 @@
+package model.data;
+
+import com.binance.api.client.domain.market.Candlestick;
+import com.binance.api.client.domain.market.CandlestickInterval;
+import com.binance.api.client.domain.market.TickerStatistics;
+import model.DataIdentifier;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MindData {
+
+	public HashMap<DataIdentifier, List<Candlestick>> candlestickData;
+	public HashMap<DataIdentifier, TickerStatistics> lastPriceData;
+	public HashMap<DataIdentifier, Double> candlestickIntAvgData;
+
+	public MindData() {
+		candlestickData = new HashMap<DataIdentifier, List<Candlestick>>();
+		lastPriceData = new HashMap<DataIdentifier, TickerStatistics>();
+		candlestickIntAvgData = new HashMap<DataIdentifier, Double>();
+	}
+
+	public Map<DataIdentifier, List<Candlestick>> getCandlestickData() {
+		return candlestickData;
+	}
+
+	public void setCandlestickData(HashMap<DataIdentifier, List<Candlestick>> candlestickData) {
+		this.candlestickData = candlestickData;
+	}
+
+	public HashMap<DataIdentifier, TickerStatistics> getLastPriceData() {
+		return lastPriceData;
+	}
+
+	public void setLastPriceData(HashMap<DataIdentifier, TickerStatistics> lastPriceData) {
+		this.lastPriceData = lastPriceData;
+	}
+
+	public HashMap<DataIdentifier, Double> getCandlestickIntAvgData() {
+		return candlestickIntAvgData;
+	}
+
+	public void setCandlestickIntAvgData(HashMap<DataIdentifier, Double> candlestickIntAvgData) {
+		this.candlestickIntAvgData = candlestickIntAvgData;
+	}
+}
