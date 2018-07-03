2c713089429b8aac2a2fd24474747e527083cb4e
Tongjian Cui <67202237+elseifn@users.noreply.github.com>
2018-07-03 20:41:58 +0800

    Added class to help with data prediction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,11 @@
 import mind.Santini;
-import model.data.MindData;
-import model.data.PredictionData;
 
 public class Main {
 	public static void main(String[] args) {
 		Santini dolores = new Santini();
 		//Santini.playSweetWater();
-		MindData theCradle = dolores.gatherData();
-		PredictionData predictionData = new PredictionData(theCradle);
-		predictionData.calculatePredictionData();
+		dolores.gatherMindData();
+		dolores.gatherPredictionData();
+
 	}
 }
