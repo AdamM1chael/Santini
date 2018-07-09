11e381d9f9a3156e713a4c416e4553748cb0c17a
Tongjian Cui <67202237+elseifn@users.noreply.github.com>
2018-07-10 03:57:57 +0800

    Stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,27 @@
+import com.binance.api.client.domain.market.CandlestickInterval;
 import mind.Santini;
+import model.DataIdentifier;
+import utils.CalcUtils;
+
+import java.util.HashMap;
 
 public class Main {
 	public static void main(String[] args) {
-		Santini dolores = new Santini();
+
+		if(args[0] == null || args[1] == null) System.exit(-1);
+		if(args[0].equals("") || args[1].equals("")) System.exit(-2);
+
 		Santini.playSweetWater();
-		dolores.gatherMindData();
-		dolores.gatherPredictionData();
+
+		for(;;) {
+			Santini dolores = new Santini(args[0], args[1]);
+			dolores.gatherMindData();
+			dolores.gatherPredictionData();
+			if (dolores.predictionData.sellConfidencePercentage > 85.0) {
+				dolores.performSellAndBuyBack();
+			} else {
+				new CalcUtils().sleeper(30000);
+			}
+		}
 	}
 }
