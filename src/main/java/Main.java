4aeba68408363e9939ac59d29e3644d23681caf1
Tongjian Cui <67202237+elseifn@users.noreply.github.com>
2018-09-03 01:47:05 +0800

    Now trades more BTC if it can and logs more

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		if (args[0] == null || args[1] == null) System.exit(-1);
 		if (args[0].equals("") || args[1].equals("")) System.exit(-2);
 		//Santini.playSweetWater();
+		System.out.println("Starting SANTINI version 1.0.2 ...");
 		for (; ; ) {
 			Santini dolores = new Santini(args[0], args[1]);
 			dolores.gatherMindData();
