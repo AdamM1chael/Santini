a0756de1f3dc95537c6f00ccda85ba0e52b64aa7
Tongjian Cui <67202237+elseifn@users.noreply.github.com>
2018-12-24 12:03:42 +0800

    Santini now tweets

--- src/main/java/mind/Santini.java
+++ src/main/java/mind/Santini.java
@@ -147,6 +147,7 @@ public class Santini {
                 Twitter twitter = TwitterFactory.getSingleton();
                 try {
                     Status status = twitter.updateStatus(message);
+                    logger.trace("Sent tweet to @Santini__Dolores");
                 } catch (TwitterException e) {
                     logger.error("ERROR SENDING TWEET: Reason: {}", e);
                 }
