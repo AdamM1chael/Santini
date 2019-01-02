135a10ce4e8bcbf06cfa73796de2475613c4e862
Tongjian Cui <67202237+elseifn@users.noreply.github.com>
2019-01-03 01:33:38 +0800

    CADEStraining

--- src/main/java/mind/Santini.java
+++ src/main/java/mind/Santini.java
@@ -32,7 +32,7 @@ import static com.binance.api.client.domain.account.NewOrder.limitSell;
 public class Santini {
 
     final static Logger logger = Logger.getLogger(Santini.class);
-    private static Double percentageRatio = 1.0017;
+    private static Double percentageRatio = 1.00165;
     private static int MAX_TRADES_PER_24HOURS = 10;
     private static CandlestickInterval[] intervalList = {
             CandlestickInterval.ONE_MINUTE};
