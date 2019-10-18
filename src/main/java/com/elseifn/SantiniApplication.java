c9d6d2a4c352d19a7a4ef6a1068b8a6846436cd4
Tongjian Cui <67202237+elseifn@users.noreply.github.com>
2019-10-18 18:35:32 +0800

    Basically, shit works now

--- src/main/java/com/elseifn/SantiniApplication.java
+++ src/main/java/com/elseifn/SantiniApplication.java
@@ -3,15 +3,17 @@ package com.elseifn;
 import com.elseifn.santini.mind.Santini;
 import com.elseifn.santini.utils.CalcUtils;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SantiniApplication {
   private final static Logger logger = Logger.getLogger(SantiniApplication.class);
 
   public static void main(String[] args) {
-    SpringApplication.run(SantiniApplication.class, args);
+    ConfigurableApplicationContext context = SpringApplication.run(SantiniApplication.class, args);
     logger.info("Starting SANTINI (v6.0.0) ...");
     if (args.length < 6) {
       logger.error("Not enough arguments have been given");
@@ -19,7 +21,8 @@ public class SantiniApplication {
     }
     logger.info("Starting Santini trading...");
     for (;;) {
-      Santini dolores = new Santini(args[0], args[1]);
+      Santini dolores = context.getBean(Santini.class);
+      dolores.setBinanceCreds(args[0], args[1]);
       dolores.setTwitterCreds(args[2], args[3], args[4], args[5]);
       dolores.gatherMindData();
       dolores.predictAndTrade();
