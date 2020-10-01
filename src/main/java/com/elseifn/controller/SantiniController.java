cb490233aaa798644c98e47e9f8a658d1d577988
Tongjian Cui <67202237+elseifn@users.noreply.github.com>
2020-10-01 08:16:48 +0800

    Working on new average load time (thats what she said)

--- src/main/java/com/elseifn/controller/SantiniController.java
+++ src/main/java/com/elseifn/controller/SantiniController.java
@@ -1,5 +1,6 @@
 package com.elseifn.controller;
 
+import com.google.common.collect.EvictingQueue;
 import com.google.common.hash.Hashing;
 import com.elseifn.santini.mind.Santini;
 import org.apache.log4j.Logger;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
+import java.util.Queue;
 
 @RestController
 public class SantiniController {
@@ -23,10 +25,12 @@ public class SantiniController {
   private static final String PATH_ORDER_HISTORY = "/orders";
   private static final String RESPONSE_SUFFIX = " endpoint hit";
   private final Santini santini;
+  private Queue<Double> queue;
 
   @Autowired
   public SantiniController(Santini santini) {
     this.santini = santini;
+    queue = EvictingQueue.create(10);
   }
 
   @GetMapping(path = PATH_BALANCE)
@@ -142,8 +146,9 @@ public class SantiniController {
     response +=
         "<br>Santini: <a href=\"https://www.blockchain.com/btc/address/"
             + "1BWu4LtW1swREcDWffFHZSuK3VTT1iWuba\" style=\"color:#F7931A\">1BW...uba</a>";
-    Double duration = (System.nanoTime() - startTime);
-    logger.debug("took " + String.format("%.5f", duration/1000000000) + " seconds");
+    Double duration = (System.nanoTime() - startTime)/1000000000;
+    queue.add(duration);
+    response += "<br><br><m>" + String.format("%.5f", getAverageStatusLoadTime()) + "</m>";
     return new ResponseEntity<>(
         "<html>"
             + "<head>"
@@ -199,5 +204,16 @@ public class SantiniController {
             + "</body>"
             + "</html>",
         HttpStatus.OK);
+  }
+
+  private Double getAverageStatusLoadTime() {
+    if (queue.size() == 0) {
+      return null;
+    }
+    Double average = 0.0;
+    for(Double num : queue) {
+      average += num / queue.size();
+    }
+    return average;
   }
 }
